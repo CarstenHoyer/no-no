aa7b704d2907a560e5e7c4c74bab4cdd059bce3f
Carsten Høyer <carsten.hoyer@akqa.com>
2024-05-18 15:42:28 +0200

    add tonemapping

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,33 @@ import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import { Scene } from "./Scene";
 import { OrbitControls } from "@react-three/drei";
+import { ToneMapping } from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import { LinearToneMapping } from "three";
 
 function App() {
   return (
-    <>
-      <Canvas flat linear shadows>
-        <ambientLight intensity={5} />
-        <OrbitControls />
-        <Scene />
-      </Canvas>
-    </>
+    <Canvas
+      shadows
+      gl={{
+        toneMapping: LinearToneMapping,
+        toneMappingExposure: Math.pow(2, 0),
+      }}
+    >
+      <ToneMapping
+        blendFunction={BlendFunction.NORMAL} // blend mode
+        adaptive={true} // toggle adaptive luminance map usage
+        resolution={256} // texture resolution of the luminance map
+        middleGrey={0.6} // middle grey factor
+        maxLuminance={16.0} // maximum luminance
+        averageLuminance={1.0} // average luminance
+        adaptationRate={1.0} // luminance adaptation rate
+      />
+      <ambientLight intensity={1} />
+      <directionalLight position={[0.5, 0, 0.866]} intensity={4} />
+      <OrbitControls />
+      <Scene />
+    </Canvas>
   );
 }
 
